8ccf9ddb59627092c6ad1d618cf3a1a6b6a80581
Remco Blom <rblom@sogyo.nl>
2017-08-22 09:11:28 +0200

    added methods to find books in database, changed pom to build in jenkins

--- src/main/java/com/model/Library.java
+++ src/main/java/com/model/Library.java
@@ -1,5 +1,6 @@
 package com.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.persistence.DatabaseHandler;
@@ -12,5 +13,24 @@ public class Library {
 	
 	public static boolean addBook(Book book) {
 		return DatabaseHandler.addBook(book);
+	}
+	
+	public static List<Book> getBooks(String titleInput, 
+			String authorInput, String isbnInput) {
+		ISBN isbn;
+		try {
+			isbn = new ISBN(isbnInput);
+		} catch (IllegalArgumentException e) {
+			return new ArrayList<Book>();
+		}
+		return DatabaseHandler.getBooks(titleInput, authorInput, isbn);
+	}
+	
+	private boolean isbnIsValid(String isbnInput) {
+		return stringHasOnlyNumbers(isbnInput) && stringHasThirteenChars(isbnInput);
+	}
+	
+	private boolean stringHasOnlyNumbers(String isbnInput) {
+		return
 	}
 }
--- src/main/java/com/rest/BookResource.java
+++ src/main/java/com/rest/BookResource.java
@@ -2,8 +2,13 @@ package com.rest;
 
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+
+import java.util.List;
+
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 
 import com.model.Book;
 import com.model.Library;
@@ -11,6 +16,14 @@ import com.model.SuccessMessage;
 
 @Path("book")
 public class BookResource {
+	
+	@GET
+	@Produces("application/json")
+	public List<Book> getBooks(@QueryParam("title") String titleInput,
+			@QueryParam("author") String authorInput, 
+			@QueryParam("isbn") String isbnInput) {
+		return Library.getBooks(titleInput, authorInput, isbnInput);
+	}
 
 	@POST @Path("/add")
 	@Consumes("application/json")
