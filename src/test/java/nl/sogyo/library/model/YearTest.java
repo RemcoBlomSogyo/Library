4e3b6f6d620146621f230d9be0425fe8c5bb41ea
Remco Blom <rblom@sogyo.nl>
2017-09-25 16:30:49 +0200

    wrote tests for year validator

---
+++ src/test/java/nl/sogyo/library/model/YearTest.java
@@ -0,0 +1,30 @@
+package nl.sogyo.library.model;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import nl.sogyo.library.model.helper.InputValidator;
+
+public class YearTest {
+	
+	@Test
+	public void year1999IsValidYear() {
+		String year = "1999";
+		boolean result = InputValidator.validateYear(year);
+		Assert.assertTrue(result);
+	}
+	
+	@Test
+	public void year3261IsNotValidYear() {
+		String year = "3261";
+		boolean result = InputValidator.validateYear(year);
+		Assert.assertFalse(result);
+	}
+	
+	@Test
+	public void yearWithLettersAndNumbersIsNotValidYear() {
+		String year = "u5l1j8";
+		boolean result = InputValidator.validateYear(year);
+		Assert.assertFalse(result);
+	}
+}
