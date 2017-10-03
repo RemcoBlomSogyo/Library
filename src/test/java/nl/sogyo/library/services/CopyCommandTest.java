9625899b01434423a611e1a8962f81671b3f5ca2
Remco Blom <rblom@sogyo.nl>
2017-10-03 15:45:26 +0200

    added tests for adding and deleting copies via rest api

--- src/test/java/nl/sogyo/library/services/CopyCommandTest.java
+++ src/test/java/nl/sogyo/library/services/CopyCommandTest.java
@@ -1,5 +1,71 @@
 package nl.sogyo.library.services;
 
-public class CopyCommandTest {
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.Response;
 
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.test.JerseyTest;
+import org.glassfish.jersey.test.TestProperties;
+import org.junit.Assert;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+import nl.sogyo.library.services.rest.libraryapi.json.BookFormInput;
+import nl.sogyo.library.services.rest.libraryapi.json.message.AddBookMessage;
+import nl.sogyo.library.services.rest.libraryapi.json.message.AddCopyMessage;
+import nl.sogyo.library.services.rest.libraryapi.json.message.DeleteCopyMessage;
+import nl.sogyo.library.services.rest.libraryapi.resources.BookResource;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class CopyCommandTest extends JerseyTest {
+	
+	@Override
+	public Application configure() {
+		enable(TestProperties.LOG_TRAFFIC);
+		enable(TestProperties.DUMP_ENTITY);
+		return new ResourceConfig(BookResource.class);
+	}
+	
+	@Test
+	public void test01AddCopyOfProRESTfulAPIs() {
+	    Response response = target("book").path("46/copy").request().post(null);
+	    AddCopyMessage addCopyMessage = response.readEntity(AddCopyMessage.class);
+	    Assert.assertTrue(addCopyMessage.getCommandSucceeded());
+	}
+	
+	@Test
+	public void test02DeleteCopyOfProRESTfulAPIs() {
+	    Response response = target("book").path("46/copy").request().delete();
+	    DeleteCopyMessage deleteCopyMessage = response.readEntity(DeleteCopyMessage.class);
+	    Assert.assertTrue(deleteCopyMessage.getCommandSucceeded());
+	}
+	
+	@Test
+	public void test03AddCopyOfNotExistingBook() {
+	    Response response = target("book").path("10000000/copy").request().post(null);
+	    AddCopyMessage addCopyMessage = response.readEntity(AddCopyMessage.class);
+	    Assert.assertFalse(addCopyMessage.getCommandSucceeded());
+	}
+	
+	@Test
+	public void test04DeleteCopyOfNotExistingBook() {
+	    Response response = target("book").path("10000000/copy").request().delete();
+	    DeleteCopyMessage deleteCopyMessage = response.readEntity(DeleteCopyMessage.class);
+	    Assert.assertFalse(deleteCopyMessage.getCommandSucceeded());
+	}
+	
+	@Test
+	public void test05DeleteCopyOfBookWithNoCopies() {
+		BookFormInput bookFormInput = new BookFormInput("title", "Sanjay", "Patni", "REST", "Appress", "9784567890120");
+	    Response addBookResponse = target("book").request().post(Entity.json(bookFormInput));
+	    AddBookMessage addBookMessage = addBookResponse.readEntity(AddBookMessage.class);
+	    
+	    Response deleteCopyResponse = target("book").path(addBookMessage.getBookId() + "/copy").request().delete();
+	    DeleteCopyMessage deleteCopyMessage = deleteCopyResponse.readEntity(DeleteCopyMessage.class);
+	    
+	    target("book").path(Integer.toString(addBookMessage.getBookId())).request().delete();
+	    Assert.assertFalse(deleteCopyMessage.getCommandSucceeded());
+	}
 }
