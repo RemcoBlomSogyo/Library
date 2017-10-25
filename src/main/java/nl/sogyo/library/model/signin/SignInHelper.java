e5603c6682096ec562f3b94178c7b88a6070ca1e
Remco Blom <rblom@sogyo.nl>
2017-10-25 13:41:05 +0200

    refactored signinhelper, google oauth is now in seperate project, got jar via maven

--- src/main/java/nl/sogyo/library/model/signin/SignInHelper.java
+++ src/main/java/nl/sogyo/library/model/signin/SignInHelper.java
@@ -1,26 +1,20 @@
 package nl.sogyo.library.model.signin;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.File;
 import java.io.IOException;
-
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeTokenRequest;
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
-import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
-import com.google.api.client.googleapis.auth.oauth2.GoogleTokenResponse;
-import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.json.jackson2.JacksonFactory;
 
 import nl.sogyo.library.model.entity.User;
 import nl.sogyo.library.persistence.DatabaseHandler;
 import nl.sogyo.library.services.rest.libraryapi.json.SignInCode;
+import nl.sogyo.oauth.javagooglesignin.EmailNotVerifiedException;
+import nl.sogyo.oauth.javagooglesignin.GoogleUser;
+import nl.sogyo.oauth.javagooglesignin.OauthHelper;
 
 public class SignInHelper {
 
 	private DatabaseHandler databaseHandler;
 	private static final String CLIENT_SECRET_FILE = "C:/Users/rblom/Documents/apache-tomcat/webapps/SogyoLibrary/src/main/resources/"
 			+ "client_secret_1000284014442-khpk4ottrb9c0njde9ho3nbqrkdo7p99.apps.googleusercontent.com.json";
-	private static final String TOKEN_SERVER_ENCODED_URL = "https://www.googleapis.com/oauth2/v4/token";
 	private static final String REDIRECT_URI = "http://localhost:8081";
 	
 	public SignInHelper() {
@@ -28,63 +22,32 @@ public class SignInHelper {
 	}
 	
 	public int signInUser(SignInCode signInCode) {
-		User googleUser = null;
+		GoogleUser googleUser = null;
+		String authorizationCode = signInCode.getCode();
 		try {
-			googleUser = getUserFromGoogle(signInCode);
-		} catch (FileNotFoundException e) {
-			// client secret file has not been found
-			return 1;
+			File file = new File(CLIENT_SECRET_FILE);
+			OauthHelper oauthHelper = new OauthHelper(file, REDIRECT_URI);
+			googleUser = oauthHelper.getUserFromGoogle(authorizationCode);
 		} catch (IOException e) {
 			// google has not sent a token
+			return 1;
+		} catch (EmailNotVerifiedException e) {
 			return 2;
+		}
+		
+		User sogyoUser = null;
+		String userName = googleUser.getGivenName() + " " + googleUser.getFamilyName();
+		try {
+			sogyoUser = new User(googleUser.getUserId(), userName, googleUser.getEmail());
 		} catch (IllegalArgumentException e) {
-			// google user is not sogyo account
 			return 3;
-		} catch (RuntimeException e) {
-			// user is not verified by google
-			return 4;
 		}
 		
-		User libraryUser = databaseHandler.getUserWithId(googleUser);
+		User libraryUser = databaseHandler.getUserWithId(sogyoUser);
 		if (libraryUser == null) {
-			libraryUser = databaseHandler.createProfile(googleUser);
+			libraryUser = databaseHandler.createProfile(sogyoUser);
 		}
 
-		
-		
-		
 		return 0;
-	}
-	
-	private User getUserFromGoogle(SignInCode signInCode) throws IOException {
-		String code = signInCode.getCode();
-	
-		GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JacksonFactory.getDefaultInstance(), 
-				new FileReader(CLIENT_SECRET_FILE));
-		GoogleTokenResponse tokenResponse = new GoogleAuthorizationCodeTokenRequest(
-				new NetHttpTransport(),
-				JacksonFactory.getDefaultInstance(),
-				TOKEN_SERVER_ENCODED_URL,
-				clientSecrets.getDetails().getClientId(),
-				clientSecrets.getDetails().getClientSecret(),
-				code,
-				REDIRECT_URI)  
-				.execute();
-		GoogleIdToken idToken = tokenResponse.parseIdToken();
-		GoogleIdToken.Payload payload = idToken.getPayload();
-
-		if (isEmailVerified(payload)) {
-			String googleUserId = payload.getSubject();
-			String email = payload.getEmail();
-			String name = (String) payload.get("name");
-			User user = new User(googleUserId, name, email);
-			return user;
-		} else {
-			throw new RuntimeException("Email is not verified by Google");
-		}
-	}
-	
-	private boolean isEmailVerified(GoogleIdToken.Payload payload) {
-		return Boolean.valueOf(payload.getEmailVerified());
 	}
 }
