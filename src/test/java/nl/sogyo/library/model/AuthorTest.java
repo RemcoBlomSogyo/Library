89901ea1a3d88fcafdb259080283507c7f4eddb2
Remco Blom <rblom@sogyo.nl>
2017-10-30 08:59:46 +0100

    changed cobertura in pom, commented unit tests to test if cobertura is still working

--- src/test/java/nl/sogyo/library/model/AuthorTest.java
+++ src/test/java/nl/sogyo/library/model/AuthorTest.java
@@ -6,18 +6,18 @@ import nl.sogyo.library.model.entity.Author;
 
 public class AuthorTest {
 	
-	@Test (expected = IllegalArgumentException.class)
-	public void emptyForenameGivesException() {
-		new Author("", "surname");
-	}
-	
-	@Test (expected = IllegalArgumentException.class)
-	public void emptySurnameGivesException() {
-		new Author("forname", "");
-	}
-	
-	@Test (expected = IllegalArgumentException.class)
-	public void emptyNamesGivesException() {
-		new Author("", "");
-	}
+//	@Test (expected = IllegalArgumentException.class)
+//	public void emptyForenameGivesException() {
+//		new Author("", "surname");
+//	}
+//	
+//	@Test (expected = IllegalArgumentException.class)
+//	public void emptySurnameGivesException() {
+//		new Author("forname", "");
+//	}
+//	
+//	@Test (expected = IllegalArgumentException.class)
+//	public void emptyNamesGivesException() {
+//		new Author("", "");
+//	}
 }
--- src/test/java/nl/sogyo/library/model/IsbnTest.java
+++ src/test/java/nl/sogyo/library/model/IsbnTest.java
@@ -7,59 +7,59 @@ import nl.sogyo.library.model.helper.InputValidator;
 
 public class IsbnTest {
 
-	@Test
-	public void isbnOfGettingStartedWithPolymerIsCorrect() {
-		String isbn = "9781785889370";
-		boolean result = InputValidator.validateIsbn(isbn);
-		Assert.assertTrue(result);
-	}
-	
-	@Test
-	public void isbnOfGettingStartedWithPolymerWithOtherCheckNumberIsIncorrect() {
-		String isbn = "9781785889375";
-		boolean result = InputValidator.validateIsbn(isbn);
-		Assert.assertFalse(result);
-	}
-	
-	@Test
-	public void isbnOfProRestfulApisIsCorrect() {
-		String isbn = "9781484226643";
-		boolean result = InputValidator.validateIsbn(isbn);
-		Assert.assertTrue(result);
-	}
-	
-	@Test
-	public void isbnOfProRestfulApisWithOtherCheckNumberIsIncorrect() {
-		String isbn = "9781484226647";
-		boolean result = InputValidator.validateIsbn(isbn);
-		Assert.assertFalse(result);
-	}
-	
-	@Test
-	public void emptyStringIsIncorrect() {
-		String isbn = "";
-		boolean result = InputValidator.validateIsbn(isbn);
-		Assert.assertFalse(result);
-	}
-	
-	@Test
-	public void stringWithTenRandomNumbersAndLettersIsIncorrect() {
-		String isbn = "1y4l2q9b8j";
-		boolean result = InputValidator.validateIsbn(isbn);
-		Assert.assertFalse(result);
-	}
-	
-	@Test
-	public void stringWithThirteenRandomNumbersAndLettersIsIncorrect() {
-		String isbn = "152370usgje";
-		boolean result = InputValidator.validateIsbn(isbn);
-		Assert.assertFalse(result);
-	}
-	
-	@Test
-	public void stringWithTenRandomNumbersIsIncorrect() {
-		String isbn = "1234567890";
-		boolean result = InputValidator.validateIsbn(isbn);
-		Assert.assertFalse(result);
-	}
+//	@Test
+//	public void isbnOfGettingStartedWithPolymerIsCorrect() {
+//		String isbn = "9781785889370";
+//		boolean result = InputValidator.validateIsbn(isbn);
+//		Assert.assertTrue(result);
+//	}
+//	
+//	@Test
+//	public void isbnOfGettingStartedWithPolymerWithOtherCheckNumberIsIncorrect() {
+//		String isbn = "9781785889375";
+//		boolean result = InputValidator.validateIsbn(isbn);
+//		Assert.assertFalse(result);
+//	}
+//	
+//	@Test
+//	public void isbnOfProRestfulApisIsCorrect() {
+//		String isbn = "9781484226643";
+//		boolean result = InputValidator.validateIsbn(isbn);
+//		Assert.assertTrue(result);
+//	}
+//	
+//	@Test
+//	public void isbnOfProRestfulApisWithOtherCheckNumberIsIncorrect() {
+//		String isbn = "9781484226647";
+//		boolean result = InputValidator.validateIsbn(isbn);
+//		Assert.assertFalse(result);
+//	}
+//	
+//	@Test
+//	public void emptyStringIsIncorrect() {
+//		String isbn = "";
+//		boolean result = InputValidator.validateIsbn(isbn);
+//		Assert.assertFalse(result);
+//	}
+//	
+//	@Test
+//	public void stringWithTenRandomNumbersAndLettersIsIncorrect() {
+//		String isbn = "1y4l2q9b8j";
+//		boolean result = InputValidator.validateIsbn(isbn);
+//		Assert.assertFalse(result);
+//	}
+//	
+//	@Test
+//	public void stringWithThirteenRandomNumbersAndLettersIsIncorrect() {
+//		String isbn = "152370usgje";
+//		boolean result = InputValidator.validateIsbn(isbn);
+//		Assert.assertFalse(result);
+//	}
+//	
+//	@Test
+//	public void stringWithTenRandomNumbersIsIncorrect() {
+//		String isbn = "1234567890";
+//		boolean result = InputValidator.validateIsbn(isbn);
+//		Assert.assertFalse(result);
+//	}
 }
