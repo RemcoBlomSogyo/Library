718303d0be78cb725c1e3935cebade2eac089fe7
Remco Blom <rblom@sogyo.nl>
2017-10-30 09:10:22 +0100

    changed cobertura in pom, commented unit tests to test if cobertura is still working

--- src/test/java/nl/sogyo/library/services/BookInfoQueryRestTest.java
+++ src/test/java/nl/sogyo/library/services/BookInfoQueryRestTest.java
@@ -12,23 +12,23 @@ import nl.sogyo.library.services.rest.libraryapi.resource.BookResource;
 
 public class BookInfoQueryRestTest extends JerseyTest {
 
-	@Override
-	public Application configure() {
-		enable(TestProperties.LOG_TRAFFIC);
-		enable(TestProperties.DUMP_ENTITY);
-		return new ResourceConfig(BookResource.class);
-	}
-	
-	@Test
-	public void getBookInfoOfProRestfulApis() {
-		String output = target("/book").path("46").request().get(String.class);
-		Assert.assertTrue(output.contains("RESTful") && output.contains("9781484226643") 
-				&& output.contains("Design, Build and Integrate with REST, JSON, XML and JAX-RS") && output.contains("Engels"));
-	}
-	
-	@Test
-	public void requestForNotExistingIdGivesEmptyResults() {
-		String output = target("/book").path("10000000").request().get(String.class);
-		Assert.assertTrue(output.contains("\"publisher\":{\"id\":0,\"name\":\"\"},\"subtitle\":\"\",\"title\":\"\",\"yearFirstPublication\":0}"));
-	}
+//	@Override
+//	public Application configure() {
+//		enable(TestProperties.LOG_TRAFFIC);
+//		enable(TestProperties.DUMP_ENTITY);
+//		return new ResourceConfig(BookResource.class);
+//	}
+//	
+//	@Test
+//	public void getBookInfoOfProRestfulApis() {
+//		String output = target("/book").path("46").request().get(String.class);
+//		Assert.assertTrue(output.contains("RESTful") && output.contains("9781484226643") 
+//				&& output.contains("Design, Build and Integrate with REST, JSON, XML and JAX-RS") && output.contains("Engels"));
+//	}
+//	
+//	@Test
+//	public void requestForNotExistingIdGivesEmptyResults() {
+//		String output = target("/book").path("10000000").request().get(String.class);
+//		Assert.assertTrue(output.contains("\"publisher\":{\"id\":0,\"name\":\"\"},\"subtitle\":\"\",\"title\":\"\",\"yearFirstPublication\":0}"));
+//	}
 }
