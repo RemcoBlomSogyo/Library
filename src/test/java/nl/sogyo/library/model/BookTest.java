101386882c9019077facf196a7b5b84e0cdb626f
Remco Blom <rblom@sogyo.nl>
2017-10-30 09:27:41 +0100

    changed pom, added pluginmanagement to remove error, removed comments in unit tests

--- src/test/java/nl/sogyo/library/model/BookTest.java
+++ src/test/java/nl/sogyo/library/model/BookTest.java
@@ -12,108 +12,108 @@ import nl.sogyo.library.model.entity.Book;
 
 public class BookTest {
 	
-//	private List<Author> authors = new ArrayList<Author>();
-//	
-//	public BookTest() {
-//		authors.add(new Author("forname", "surname"));
-//	}
-//	
-//	@Test (expected = IllegalArgumentException.class)
-//	public void emptyTitleGivesException() {
-//		new Book("", "subtitle", authors, "category",
-//				"publisher", "2000", "9781484226643", "1", "language");
-//	}
-//	
-//	@Test (expected = IllegalArgumentException.class)
-//	public void emptyCategoryGivesException() {
-//		new Book("title", "subtitle", authors, "",
-//				"publisher", "2000", "9781484226643", "1", "language");
-//	}
-//	
-//	@Test (expected = IllegalArgumentException.class)
-//	public void emptyPublisherGivesException() {
-//		new Book("title", "subtitle", authors, "category",
-//				"", "2000", "9781484226643", "1", "language");
-//	}
-//	
-//	@Test
-//	public void emptySubtitleGivesNoException() {
-//		try {
-//			Book book = new Book("title", "", authors, "category",
-//				"publisher", "2000", "9781484226643", "1", "language");
-//			Assert.assertTrue(book.getSubtitle().equals(""));
-//		} catch (IllegalArgumentException e) {
-//			Assert.fail("Empty subtitle gives illegalargumentexception");
-//		}
-//	}
-//	
-//	@Test (expected = IllegalArgumentException.class)
-//	public void yearInFutureGivesException() {
-//		new Book("title", "subtitle", authors, "category",
-//				"publisher", Integer.toString(Year.now().getValue() + 1), "9781484226643", "1", "language");
-//	}
-//	
-//	@Test
-//	public void emptyYearFirstPublicationGivesNoException() {
-//		try {
-//			Book book = new Book("title", "subtitle", authors, "category",
-//				"publisher", "", "9781484226643", "1", "language");
-//			Assert.assertTrue(book.getYearFirstPublication() == 0);
-//		} catch (IllegalArgumentException e) {
-//			Assert.fail("Empty yearFirstPublication gives illegalargumentexception");
-//		}
-//	}
-//	
-//	@Test (expected = IllegalArgumentException.class)
-//	public void negativeYearGivesException() {
-//		new Book("title", "subtitle", authors, "category",
-//				"publisher", "-1", "9781484226643", "1", "language");
-//	}
-//	
-//	@Test (expected = IllegalArgumentException.class)
-//	public void yearIsNotOnlyDigitsGivesException() {
-//		new Book("title", "subtitle", authors, "category",
-//				"publisher", "e2r3l9q", "9781484226643", "1", "language");
-//	}
-//	
-//	@Test
-//	public void emptyPagesGivesNoException() {
-//		try {
-//			Book book = new Book("title", "subtitle", authors, "category",
-//				"publisher", "2000", "9781484226643", "", "language");
-//			System.out.println("testPages:" + book.getPages());
-//			Assert.assertTrue(book.getPages() == 0);
-//		} catch (IllegalArgumentException e) {
-//			Assert.fail("Empty pages gives illegalargumentexception");
-//		}
-//	}
-//	
-//	@Test (expected = IllegalArgumentException.class)
-//	public void negativePagesGivesException() {
-//		new Book("title", "subtitle", authors, "category",
-//				"publisher", "2000", "9781484226643", "-1", "language");
-//	}
-//	
-//	@Test (expected = IllegalArgumentException.class)
-//	public void pagesIsNotOnlyDigitsGivesException() {
-//		new Book("title", "subtitle", authors, "category",
-//				"publisher", "2000", "9781484226643", "1f2l8r", "language");
-//	}
-//	
-//	@Test (expected = IllegalArgumentException.class)
-//	public void NotExistingIsbnGivesException() {
-//		new Book("title", "subtitle", authors, "category",
-//				"publisher", "2000", "9781484226642", "1", "language");
-//	}
-//	
-//	@Test
-//	public void emptyLanguageGivesNoException() {
-//		try {
-//			Book book = new Book("title", "subtitle", authors, "category",
-//				"publisher", "2000", "9781484226643", "1", "");
-//			Assert.assertTrue(book.getLanguage().isEmpty());
-//		} catch (IllegalArgumentException e) {
-//			Assert.fail("Empty language gives illegalargumentexception");
-//		}
-//	}
+	private List<Author> authors = new ArrayList<Author>();
+	
+	public BookTest() {
+		authors.add(new Author("forname", "surname"));
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void emptyTitleGivesException() {
+		new Book("", "subtitle", authors, "category",
+				"publisher", "2000", "9781484226643", "1", "language");
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void emptyCategoryGivesException() {
+		new Book("title", "subtitle", authors, "",
+				"publisher", "2000", "9781484226643", "1", "language");
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void emptyPublisherGivesException() {
+		new Book("title", "subtitle", authors, "category",
+				"", "2000", "9781484226643", "1", "language");
+	}
+	
+	@Test
+	public void emptySubtitleGivesNoException() {
+		try {
+			Book book = new Book("title", "", authors, "category",
+				"publisher", "2000", "9781484226643", "1", "language");
+			Assert.assertTrue(book.getSubtitle().equals(""));
+		} catch (IllegalArgumentException e) {
+			Assert.fail("Empty subtitle gives illegalargumentexception");
+		}
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void yearInFutureGivesException() {
+		new Book("title", "subtitle", authors, "category",
+				"publisher", Integer.toString(Year.now().getValue() + 1), "9781484226643", "1", "language");
+	}
+	
+	@Test
+	public void emptyYearFirstPublicationGivesNoException() {
+		try {
+			Book book = new Book("title", "subtitle", authors, "category",
+				"publisher", "", "9781484226643", "1", "language");
+			Assert.assertTrue(book.getYearFirstPublication() == 0);
+		} catch (IllegalArgumentException e) {
+			Assert.fail("Empty yearFirstPublication gives illegalargumentexception");
+		}
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void negativeYearGivesException() {
+		new Book("title", "subtitle", authors, "category",
+				"publisher", "-1", "9781484226643", "1", "language");
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void yearIsNotOnlyDigitsGivesException() {
+		new Book("title", "subtitle", authors, "category",
+				"publisher", "e2r3l9q", "9781484226643", "1", "language");
+	}
+	
+	@Test
+	public void emptyPagesGivesNoException() {
+		try {
+			Book book = new Book("title", "subtitle", authors, "category",
+				"publisher", "2000", "9781484226643", "", "language");
+			System.out.println("testPages:" + book.getPages());
+			Assert.assertTrue(book.getPages() == 0);
+		} catch (IllegalArgumentException e) {
+			Assert.fail("Empty pages gives illegalargumentexception");
+		}
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void negativePagesGivesException() {
+		new Book("title", "subtitle", authors, "category",
+				"publisher", "2000", "9781484226643", "-1", "language");
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void pagesIsNotOnlyDigitsGivesException() {
+		new Book("title", "subtitle", authors, "category",
+				"publisher", "2000", "9781484226643", "1f2l8r", "language");
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void NotExistingIsbnGivesException() {
+		new Book("title", "subtitle", authors, "category",
+				"publisher", "2000", "9781484226642", "1", "language");
+	}
+	
+	@Test
+	public void emptyLanguageGivesNoException() {
+		try {
+			Book book = new Book("title", "subtitle", authors, "category",
+				"publisher", "2000", "9781484226643", "1", "");
+			Assert.assertTrue(book.getLanguage().isEmpty());
+		} catch (IllegalArgumentException e) {
+			Assert.fail("Empty language gives illegalargumentexception");
+		}
+	}
 }
--- src/test/java/nl/sogyo/library/services/CopyCommandRestTest.java
+++ src/test/java/nl/sogyo/library/services/CopyCommandRestTest.java
@@ -20,52 +20,52 @@ import nl.sogyo.library.services.rest.libraryapi.resource.BookResource;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class CopyCommandRestTest extends JerseyTest {
-//	
-//	@Override
-//	public Application configure() {
-//		enable(TestProperties.LOG_TRAFFIC);
-//		enable(TestProperties.DUMP_ENTITY);
-//		return new ResourceConfig(BookResource.class);
-//	}
-//	
-//	@Test
-//	public void test01AddCopyOfProRESTfulAPIs() {
-//	    Response response = target("book").path("46/copy").request().post(null);
-//	    AddCopyMessage addCopyMessage = response.readEntity(AddCopyMessage.class);
-//	    Assert.assertTrue(addCopyMessage.getCommandSucceeded());
-//	}
-//	
-//	@Test
-//	public void test02DeleteCopyOfProRESTfulAPIs() {
-//	    Response response = target("book").path("46/copy").request().delete();
-//	    DeleteCopyMessage deleteCopyMessage = response.readEntity(DeleteCopyMessage.class);
-//	    Assert.assertTrue(deleteCopyMessage.getCommandSucceeded());
-//	}
-//	
-//	@Test
-//	public void test03AddCopyOfNotExistingBook() {
-//	    Response response = target("book").path("10000000/copy").request().post(null);
-//	    AddCopyMessage addCopyMessage = response.readEntity(AddCopyMessage.class);
-//	    Assert.assertFalse(addCopyMessage.getCommandSucceeded());
-//	}
-//	
-//	@Test
-//	public void test04DeleteCopyOfNotExistingBook() {
-//	    Response response = target("book").path("10000000/copy").request().delete();
-//	    DeleteCopyMessage deleteCopyMessage = response.readEntity(DeleteCopyMessage.class);
-//	    Assert.assertFalse(deleteCopyMessage.getCommandSucceeded());
-//	}
-//	
-//	@Test
-//	public void test05DeleteCopyOfBookWithNoCopies() {
-//		BookFormInput bookFormInput = new BookFormInput("title", "Sanjay", "Patni", "REST", "Appress", "9784567890120");
-//	    Response addBookResponse = target("book").request().post(Entity.json(bookFormInput));
-//	    AddBookMessage addBookMessage = addBookResponse.readEntity(AddBookMessage.class);
-//	    
-//	    Response deleteCopyResponse = target("book").path(addBookMessage.getBookId() + "/copy").request().delete();
-//	    DeleteCopyMessage deleteCopyMessage = deleteCopyResponse.readEntity(DeleteCopyMessage.class);
-//	    
-//	    target("book").path(Integer.toString(addBookMessage.getBookId())).request().delete();
-//	    Assert.assertFalse(deleteCopyMessage.getCommandSucceeded());
-//	}
+	
+	@Override
+	public Application configure() {
+		enable(TestProperties.LOG_TRAFFIC);
+		enable(TestProperties.DUMP_ENTITY);
+		return new ResourceConfig(BookResource.class);
+	}
+	
+	@Test
+	public void test01AddCopyOfProRESTfulAPIs() {
+	    Response response = target("book").path("46/copy").request().post(null);
+	    AddCopyMessage addCopyMessage = response.readEntity(AddCopyMessage.class);
+	    Assert.assertTrue(addCopyMessage.getCommandSucceeded());
+	}
+	
+	@Test
+	public void test02DeleteCopyOfProRESTfulAPIs() {
+	    Response response = target("book").path("46/copy").request().delete();
+	    DeleteCopyMessage deleteCopyMessage = response.readEntity(DeleteCopyMessage.class);
+	    Assert.assertTrue(deleteCopyMessage.getCommandSucceeded());
+	}
+	
+	@Test
+	public void test03AddCopyOfNotExistingBook() {
+	    Response response = target("book").path("10000000/copy").request().post(null);
+	    AddCopyMessage addCopyMessage = response.readEntity(AddCopyMessage.class);
+	    Assert.assertFalse(addCopyMessage.getCommandSucceeded());
+	}
+	
+	@Test
+	public void test04DeleteCopyOfNotExistingBook() {
+	    Response response = target("book").path("10000000/copy").request().delete();
+	    DeleteCopyMessage deleteCopyMessage = response.readEntity(DeleteCopyMessage.class);
+	    Assert.assertFalse(deleteCopyMessage.getCommandSucceeded());
+	}
+	
+	@Test
+	public void test05DeleteCopyOfBookWithNoCopies() {
+		BookFormInput bookFormInput = new BookFormInput("title", "Sanjay", "Patni", "REST", "Appress", "9784567890120");
+	    Response addBookResponse = target("book").request().post(Entity.json(bookFormInput));
+	    AddBookMessage addBookMessage = addBookResponse.readEntity(AddBookMessage.class);
+	    
+	    Response deleteCopyResponse = target("book").path(addBookMessage.getBookId() + "/copy").request().delete();
+	    DeleteCopyMessage deleteCopyMessage = deleteCopyResponse.readEntity(DeleteCopyMessage.class);
+	    
+	    target("book").path(Integer.toString(addBookMessage.getBookId())).request().delete();
+	    Assert.assertFalse(deleteCopyMessage.getCommandSucceeded());
+	}
 }
